1baffc7e19426b0685d82109afceb9b0377375c1
Vinh Ngo <s3578687@w8408160.cs.rmit.edu.au>
2018-10-25 16:40:15 +1100

    Initial Commit

---
+++ Tarot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarot
+//
+//  Created by Vinh Ngo on 25/10/18.
+//  Copyright © 2018 Vinh Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
